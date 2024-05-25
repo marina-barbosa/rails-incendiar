1fcb5597e1145ac296d44545527cbbbc7335fc93
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-05-25 00:38:36 -0300

    5 examples, 0 failures

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_141638) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_171720) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stock"
   end
 
 end
