bbfdfdf2a9515196740b601fd78525b569dd947e
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-05-25 10:18:06 -0300

    add teste unitário Product #valid?, 11 examples, 0 failures

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,4 +1,45 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Product, type: :model do
+  describe "#valid?" do
+    it "falso quando o nome está vazio" do
+      # Arrange
+      product = Product.new(
+        name: "",
+        description: "Isqueiro à prova de vento com garantia vitalícia. Modelo clássico da Zippo.",
+        price: 150.00,
+        stock: 30,
+      )
+      # Act
+      result = product.valid?
+      # Assert
+      expect(result).to eq false
+    end
+    it "falso quando o preço está vazio" do
+      # Arrange
+      product = Product.new(
+        name: "Isqueiro Clássico Zippo",
+        description: "Isqueiro à prova de vento com garantia vitalícia. Modelo clássico da Zippo.",
+        price: "",
+        stock: 30,
+      )
+      # Act
+      result = product.valid?
+      # Assert
+      expect(result).to eq false
+    end
+    it "falso quando o estoque está vazio" do
+      # Arrange
+      product = Product.new(
+        name: "Isqueiro Clássico Zippo",
+        description: "Isqueiro à prova de vento com garantia vitalícia. Modelo clássico da Zippo.",
+        price: 150.00,
+        stock: "",
+      )
+      # Act
+      result = product.valid?
+      # Assert
+      expect(result).to eq false
+    end
+  end
 end
