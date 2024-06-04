91d52b4001a5e4f34dba9c5f5803139416647108
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-06-04 19:53:07 -0300

    add before_action

--- app/controllers/ProductsController.rb
+++ app/controllers/ProductsController.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_admin, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
