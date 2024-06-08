8baf56f0178f6698ba6b60c9ab631bdbaf2a1af3
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-06-07 21:50:25 -0300

    42 examples, 11 failures

---
+++ spec/models/address_spec.rb
@@ -0,0 +1,157 @@
+require "rails_helper"
+
+RSpec.describe Address, type: :model do
+  describe "#valid?" do
+    it "falso quando a rua está vazia" do
+      # Arrange
+      address = Address.new(
+        street: "",
+        number: "123",
+        neighborhood: "Centro",
+        city: "São Paulo",
+        state: "SP",
+        postal_code: "12345-678",
+        country: "Brasil",
+        complement: "Apto 101",
+        user: User.new,
+      )
+      # Act
+      result = address.valid?
+      # Assert
+      expect(result).to eq false
+    end
+
+    it "falso quando o número está vazio" do
+      # Arrange
+      address = Address.new(
+        street: "Avenida Paulista",
+        number: "",
+        neighborhood: "Centro",
+        city: "São Paulo",
+        state: "SP",
+        postal_code: "12345-678",
+        country: "Brasil",
+        complement: "Apto 101",
+        user: User.new,
+      )
+      # Act
+      result = address.valid?
+      # Assert
+      expect(result).to eq false
+    end
+
+    it "falso quando o bairro está vazio" do
+      # Arrange
+      address = Address.new(
+        street: "Avenida Paulista",
+        number: "123",
+        neighborhood: "",
+        city: "São Paulo",
+        state: "SP",
+        postal_code: "12345-678",
+        country: "Brasil",
+        complement: "Apto 101",
+        user: User.new,
+      )
+      # Act
+      result = address.valid?
+      # Assert
+      expect(result).to eq false
+    end
+
+    it "falso quando a cidade está vazia" do
+      # Arrange
+      address = Address.new(
+        street: "Avenida Paulista",
+        number: "123",
+        neighborhood: "Centro",
+        city: "",
+        state: "SP",
+        postal_code: "12345-678",
+        country: "Brasil",
+        complement: "Apto 101",
+        user: User.new,
+      )
+      # Act
+      result = address.valid?
+      # Assert
+      expect(result).to eq false
+    end
+
+    it "falso quando o estado está vazio" do
+      # Arrange
+      address = Address.new(
+        street: "Avenida Paulista",
+        number: "123",
+        neighborhood: "Centro",
+        city: "São Paulo",
+        state: "",
+        postal_code: "12345-678",
+        country: "Brasil",
+        complement: "Apto 101",
+        user: User.new,
+      )
+      # Act
+      result = address.valid?
+      # Assert
+      expect(result).to eq false
+    end
+
+    it "falso quando o CEP está vazio" do
+      # Arrange
+      address = Address.new(
+        street: "Avenida Paulista",
+        number: "123",
+        neighborhood: "Centro",
+        city: "São Paulo",
+        state: "SP",
+        postal_code: "",
+        country: "Brasil",
+        complement: "Apto 101",
+        user: User.new,
+      )
+      # Act
+      result = address.valid?
+      # Assert
+      expect(result).to eq false
+    end
+
+    it "falso quando o país está vazio" do
+      # Arrange
+      address = Address.new(
+        street: "Avenida Paulista",
+        number: "123",
+        neighborhood: "Centro",
+        city: "São Paulo",
+        state: "SP",
+        postal_code: "12345-678",
+        country: "",
+        complement: "Apto 101",
+        user: User.new,
+      )
+      # Act
+      result = address.valid?
+      # Assert
+      expect(result).to eq false
+    end
+
+    it "verdadeiro quando o complemento está vazio" do
+      # Arrange
+      address = Address.new(
+        street: "Avenida Paulista",
+        number: "123",
+        neighborhood: "Centro",
+        city: "São Paulo",
+        state: "SP",
+        postal_code: "12345-678",
+        country: "Brasil",
+        complement: "",
+        user: User.new,
+      )
+      # Act
+      result = address.valid?
+      # Assert
+      expect(result).to eq true
+    end
+  end
+end
