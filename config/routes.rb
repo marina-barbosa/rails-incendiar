76fd6df503082c9d5f76a38d046fe061f2ecd3c2
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-06-07 21:30:51 -0300

    34 examples, 4 failures

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
   root "products#index"
   resources :products, only: [:show, :new, :create, :edit, :update, :destroy]
 end
