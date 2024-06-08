8d075a7da618d544ac86e2fc90316ba70b22146e
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-06-08 12:02:21 -0300

    add teste Usuário vê endereço cadastrado - 43 examples, 0 failures

--- spec/system/user_view_profile_details_spec.rb
+++ spec/system/user_view_profile_details_spec.rb
@@ -10,6 +10,17 @@ describe "Usuário vê detalhes de seu perfil" do
       phone: "13900002222",
       role: "customer",
     )
+    address = Address.create!(
+      street: "Avenida Paulista",
+      number: "123",
+      neighborhood: "Centro",
+      city: "São Paulo",
+      state: "SP",
+      postal_code: "12345-678",
+      country: "Brasil",
+      complement: "Apto 101",
+      user: @user,
+    )
   end
   it "e vê suas informações" do
     # Arrange
@@ -23,5 +34,21 @@ describe "Usuário vê detalhes de seu perfil" do
     expect(page).to have_content("isaac@email.com")
     expect(page).to have_content("22345678900")
     expect(page).to have_content("13900002222")
+  end
+  it "e vê endereço se tiver cadastrado" do
+    # Arrange
+    login_as(@user)
+    # Act
+    visit root_path
+    click_on "isaac@email.com"
+    # Assert
+    expect(page).to have_content("Avenida Paulista")
+    expect(page).to have_content("123")
+    expect(page).to have_content("Centro")
+    expect(page).to have_content("São Paulo")
+    expect(page).to have_content("SP")
+    expect(page).to have_content("12345-678")
+    expect(page).to have_content("Brasil")
+    expect(page).to have_content("Apto 101")
   end
 end
